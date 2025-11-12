1fa00f29938f6c5cb3b7c399ab5c0fb836f72bc2
alejanf2885 <alumno.648000@ies-azarquiel.es>
2025-11-12 23:05:18 +0100

    feat: agregar Speed Insights de Vercel a la aplicación

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './Router';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById("root")).render(
-    <><Analytics /><Router /></>
+    <><Analytics />
+    <SpeedInsights />
+    <Router /></>
 );
 
